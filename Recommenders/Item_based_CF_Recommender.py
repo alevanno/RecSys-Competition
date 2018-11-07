dc68fd60d7a9df79fe27a4074467cd09106324bc
Alessandro Vannoni <alevanno@live.it>
2018-11-07 19:34:06 +0100

    Added item-based CF

--- Recommenders/Item_based_CF_Recommender.py
+++ Recommenders/Item_based_CF_Recommender.py
@@ -4,15 +4,12 @@ import os
 import pandas as pd
 from scipy.sparse import coo_matrix
 from Compute_Similarity_Python import Compute_Similarity_Python
-from evaluation_function import evaluate_algorithm
-from data_splitter import train_test_holdout
-import matplotlib.pyplot as pyplot
 
 train_path = Path("data")/"train.csv"
 target_path = Path('data')/'target_playlists.csv'
 
 """
-Here a User-based CF recommender is implemented
+Here a Item-based CF recommender is implemented
 """
 ################################################################################
 class Data_matrix_utility(object):
@@ -42,7 +39,7 @@ class CF_recommender(object):
         self.urm_csr = urm_csr
 
     def fit(self, topK=50, shrink=100, normalize = True, similarity = "cosine"):
-        similarity_object = Compute_Similarity_Python(self.urm_csr.transpose(), shrink=shrink,\
+        similarity_object = Compute_Similarity_Python(self.urm_csr, shrink=shrink,\
                                                   topK=topK, normalize=normalize,\
                                                   similarity = similarity)
 #I pass the transpose of urm to calculate the similarity between playlists.
@@ -50,8 +47,8 @@ class CF_recommender(object):
         self.similarity_csr = similarity_object.compute_similarity()
 
     def recommend(self, target_id, n_tracks=None, exclude_seen=True):
-        target_profile = self.similarity_csr.getrow(target_id)
-        scores = target_profile.dot(self.urm_csr).toarray().ravel()
+        target_profile = self.urm_csr.getrow(target_id)
+        scores = target_profile.dot(self.similarity_csr).toarray().ravel()
         if exclude_seen:
             scores = self.filter_seen(target_id, scores)
 
@@ -67,45 +64,45 @@ class CF_recommender(object):
         scores[target_profile] = -np.inf
         return scores
 ################################################################################
-"""
-def provide_recommendations(k, urm):
+
+def provide_recommendations(urm):
     recommendations = {}
     urm_csr = urm.tocsr()
     targets_df = pd.read_csv(target_path)
     targets_array = targets_df.get_values().squeeze()
     recommender = CF_recommender(urm_csr)
-    recommender.fit(shrink=0, topK = k)
+    recommender.fit(shrink=0)
     for target in targets_array:
         recommendations[target] = recommender.recommend(target_id=target,n_tracks=10)
-"""
+
+    with open('item_based_CF_recommendations.csv', 'w') as f:
+        f.write('playlist_id,track_ids\n')
+        for i in sorted(recommendations):
+            f.write('{},{}\n'.format(i, ' '.join([str(x) for x in recommendations[i]])))
 
 if __name__ == '__main__':
     utility = Data_matrix_utility(train_path)
-    urm_complete = utility.build_matrix()
-    urm_train, urm_test = train_test_holdout(URM_all = urm_complete)
-    recommender = CF_recommender(urm_train)
-
-    K_values = [x for x in range(160,195,5)]
-    K_results = []
-    for k in K_values:
-        recommender.fit(topK=k,shrink=0)
-        evaluation_metrics = evaluate_algorithm(URM_test=urm_test, recommender_object=\
-                                         recommender)
-        K_results.append(evaluation_metrics["MAP"])
-    """
-    shrink_value = [for x in range(10)]
-    shrink_result = []
-    for value in shrink_value:
-        print('Evaluating shrink = ' + str(value))
-        recommender.fit(topK = ,shrink=value)
-        evaluation_metrics = evaluate_algorithm(URM_test=urm_test, recommender_object=\
-                                         recommender)
-        shrink_result.append(evaluation_metrics["MAP"])
-    """
-    pyplot.plot(K_values, K_results)
-    pyplot.ylabel('MAP')
-    pyplot.xlabel('TopK')
-    pyplot.show()
-
+    provide_recommendations(utility.build_matrix())
 
 ###############################################################################
+"""
+Results:
+- Execution time = 1.90 min (1.40 to compute similarity + 0.5 for the rest)
+- MAP on Public Test Set: 0.07657
+
+TODO:
+1. testare su un validation set differenti valori di k (del topk), plottando su un
+  grafico come varia il MAP al variare di k e usare il k che massimizzi il MAP
+2. Fare lo stesso procedimento del punto 1 per lo shrink (quindi plottare il grafico
+del MAP in funzione dello shrink e prendere lo shrink per cui il MAP del validation set
+è massimo)
+3. Provare a scrivere il CF item based recommender e paragonare le performance rispetto
+allo user-based
+
+REFERENCES
+Per i primi 2 punti guardare il notebook "Practice 3 - Content Based Filtering"
+per avere un'idea su come plottare i grafici. Per dividere il train set in train e
+validation sets importare data_splitter.py contenuto in questa stessa cartella.
+Per farsi un'idea di quali valori di k e shrink provare nei punti 1 e 2 attenersi
+a quanto detto dal prof in classe.
+"""
--- Recommenders/Item_based_CF_Recommender_with tuning.py
+++ Recommenders/Item_based_CF_Recommender_with tuning.py
@@ -12,7 +12,7 @@ train_path = Path("data")/"train.csv"
 target_path = Path('data')/'target_playlists.csv'
 
 """
-Here a User-based CF recommender is implemented
+Here a Item-based CF recommender is implemented
 """
 ################################################################################
 class Data_matrix_utility(object):
@@ -42,7 +42,7 @@ class CF_recommender(object):
         self.urm_csr = urm_csr
 
     def fit(self, topK=50, shrink=100, normalize = True, similarity = "cosine"):
-        similarity_object = Compute_Similarity_Python(self.urm_csr.transpose(), shrink=shrink,\
+        similarity_object = Compute_Similarity_Python(self.urm_csr, shrink=shrink,\
                                                   topK=topK, normalize=normalize,\
                                                   similarity = similarity)
 #I pass the transpose of urm to calculate the similarity between playlists.
@@ -50,8 +50,8 @@ class CF_recommender(object):
         self.similarity_csr = similarity_object.compute_similarity()
 
     def recommend(self, target_id, n_tracks=None, exclude_seen=True):
-        target_profile = self.similarity_csr.getrow(target_id)
-        scores = target_profile.dot(self.urm_csr).toarray().ravel()
+        target_profile = self.urm_csr.getrow(target_id)
+        scores = target_profile.dot(self.similarity_csr).toarray().ravel()
         if exclude_seen:
             scores = self.filter_seen(target_id, scores)
 
@@ -68,15 +68,20 @@ class CF_recommender(object):
         return scores
 ################################################################################
 """
-def provide_recommendations(k, urm):
+def provide_recommendations(urm):
     recommendations = {}
     urm_csr = urm.tocsr()
     targets_df = pd.read_csv(target_path)
     targets_array = targets_df.get_values().squeeze()
     recommender = CF_recommender(urm_csr)
-    recommender.fit(shrink=0, topK = k)
+    recommender.fit(shrink=0)
     for target in targets_array:
         recommendations[target] = recommender.recommend(target_id=target,n_tracks=10)
+
+    with open('item_based_CF_recommendations.csv', 'w') as f:
+        f.write('playlist_id,track_ids\n')
+        for i in sorted(recommendations):
+            f.write('{},{}\n'.format(i, ' '.join([str(x) for x in recommendations[i]])))
 """
 
 if __name__ == '__main__':
@@ -85,7 +90,7 @@ if __name__ == '__main__':
     urm_train, urm_test = train_test_holdout(URM_all = urm_complete)
     recommender = CF_recommender(urm_train)
 
-    K_values = [x for x in range(160,195,5)]
+    """K_values = [x for x in range(160,195,5)]
     K_results = []
     for k in K_values:
         recommender.fit(topK=k,shrink=0)
@@ -93,19 +98,18 @@ if __name__ == '__main__':
                                          recommender)
         K_results.append(evaluation_metrics["MAP"])
     """
-    shrink_value = [for x in range(10)]
     shrink_result = []
-    for value in shrink_value:
+    for value in range(10):
         print('Evaluating shrink = ' + str(value))
-        recommender.fit(topK = ,shrink=value)
+        recommender.fit(shrink=value)
         evaluation_metrics = evaluate_algorithm(URM_test=urm_test, recommender_object=\
                                          recommender)
         shrink_result.append(evaluation_metrics["MAP"])
-    """
+    '''
     pyplot.plot(K_values, K_results)
     pyplot.ylabel('MAP')
     pyplot.xlabel('TopK')
     pyplot.show()
-
-
+    '''
 ###############################################################################
+
